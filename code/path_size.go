04d6c2be1a6413bba10c256ef757b0ece4304dc9
s4t4nizm16 <s4t4nizm16@mail.ru>
2025-10-27 20:42:48 +0300

    Step 7: add README.md, recursive flag and refactoring tests

--- code/path_size.go
+++ code/path_size.go
@@ -7,38 +7,25 @@ import (
 
 // GetPathSize возвращает размер файлов или суммарный размер файлов в директории
 func GetPathSize(path string, recursive, human, all bool) (string, error) {
-	// заглушки для будущей логики
-	_ = recursive
-
 	info, err := os.Lstat(path)
 	if err != nil {
 		return "", fmt.Errorf("cannot access path %q: %w", path, err)
 	}
 
 	if !info.IsDir() {
+		// Если это файл, просто возвращаем его размер
 		if !isVisible(info.Name(), all) {
 			return fmt.Sprintf("%s\t%s", formatSize(0, human), path), nil
 		}
 		return fmt.Sprintf("%s\t%s", formatSize(info.Size(), human), path), nil
 	}
 
-	entries, err := os.ReadDir(path)
+	// Если это директория, начинаем подсчет (результат будет зависеть от флагов)
+	total, err := calculateDirSize(path, recursive, all)
 	if err != nil {
-		return "", fmt.Errorf("cannot read directory %q: %w", path, err)
+		return "", fmt.Errorf("error calculating directory size for %q: %w", path, err)
 	}
 
-	var total int64
-	for _, entry := range entries {
-		if !isVisible(entry.Name(), all) {
-			continue
-		}
-		if entry.Type().IsRegular() {
-			finfo, err := entry.Info()
-			if err != nil {
-				return "", fmt.Errorf("cannot get info for file %q: %w", entry.Name(), err)
-			}
-			total += finfo.Size()
-		}
-	}
+	// Возвращаем общий размер директории
 	return fmt.Sprintf("%s\t%s", formatSize(total, human), path), nil
 }
